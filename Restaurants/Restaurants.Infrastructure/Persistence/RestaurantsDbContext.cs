e8d1ff41b2198e9d3ea7df8b2a4d7fbec81bd2a9
Hasindu Shanuka <hasindur@digital400.com>
2025-07-13 22:12:46 +0530

    Create RestaurantsDbContext

---
+++ Restaurants/Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence;
+internal class RestaurantsDbContext : DbContext
+{
+    internal DbSet<Restaurant> Restaurants { get; set; }
+    internal DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Database=RestaurantsDb;Username=postgres;Password=123");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Restaurant>()
+            .OwnsOne(r => r.Address);
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(r => r.Dishes)
+            .WithOne()
+            .HasForeignKey(d => d.RestaurantId);
+    }
+}
