3c0ae82401decbb9e225888a551de369ce0298cb
Hasindu Shanuka <hasindur@digital400.com>
2025-07-26 23:03:03 +0530

    Update RestaurantDbContext

--- Restaurants/Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
+++ Restaurants/Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -2,15 +2,10 @@
 using Restaurants.Domain.Entities;
 
 namespace Restaurants.Infrastructure.Persistence;
-internal class RestaurantsDbContext : DbContext
+internal class RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> options) : DbContext(options)
 {
     internal DbSet<Restaurant> Restaurants { get; set; }
     internal DbSet<Dish> Dishes { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseNpgsql("Host=localhost;Database=RestaurantsDb;Username=postgres;Password=123");
-    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
